9418dbc219037733456503034ce3b88aca3f8f04
Nik <nik54rus@gmail.com>
2022-12-02 17:17:49 +0700

    Add update Stocks

--- src/Yandex/Marketplace/Partner/Clients/StocksClient.php
+++ src/Yandex/Marketplace/Partner/Clients/StocksClient.php
@@ -2,6 +2,7 @@
 
 namespace Yandex\Marketplace\Partner\Clients;
 
+use Yandex\Marketplace\Partner\Models\Response\PostResponse;
 use Yandex\Marketplace\Partner\Models\Response\StocksResponse;
 
 class StocksClient extends Client
@@ -19,5 +20,26 @@ class StocksClient extends Client
         $decodedResponseBody = $this->getDecodedBody($response);
 
         return new StocksResponse($decodedResponseBody);
+    }
+
+        /**
+     * @param $campaignId
+     * @param array $params
+     * @param null $dbgKey
+     * @return PostResponse
+     * @throws \Yandex\Marketplace\Partner\Exception\PartnerRequestException
+     */
+    public function updateStocks($campaignId, array $params = [], $dbgKey = null)
+    {
+        $resource = 'campaigns/' . $campaignId . '/offers/stocks.json';
+        $resource = $this->addDebugKey($resource, $dbgKey);
+        $response = $this->sendRequest(
+            'PUT',
+            $this->getServiceUrl($resource),
+            ['json' => $params]
+        );
+        $decodedResponseBody = $this->getDecodedBody($response->getBody());
+
+        return new PostResponse($decodedResponseBody);
     }
 }
